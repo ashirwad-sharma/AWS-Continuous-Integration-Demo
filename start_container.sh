8ea79260dc15139b8aaae5c8f638838d63fdd0b8
aasheervad sharma <ashirwadsharma2k@gmail.com>
2024-01-18 23:42:32 +0530

    Update start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -5,7 +5,8 @@ set -e
 docker pull ashirwad505/myfirst-python-project:latest
 # If yuo have already run this script then it will map the port 5000 to some container and if you run it again, then it will fail stating the port is 
 # already mapped so always remove the already running container 
-ContainerID= `docker ps | awk -F" "'{print$1}'`
-docker rm -f $ContainerID
+#ContainerID= `docker ps | awk -F" "'{print$1}'`
+#docker rm -f $ContainerID
 # Run the Docker image as a container
+
 docker run -d -p 5000:5000 ashirwad505/myfirst-python-project
